bcb67f7672acf3ff1c3255b03419726affd49e05
VisorOps <pliotgaming2645@gmail.com>
2025-10-04 14:05:15 +0000

    use this as the school logo

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -1,26 +1,15 @@
 import { cn } from "@/lib/utils";
-import type { SVGProps } from "react";
+import Image from "next/image";
 
-const AppLogo = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth={2}
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    className={cn("text-primary", props.className)}
-    {...props}
-  >
-    <path d="M12 22V8" />
-    <path d="M5 12H2a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h3" />
-    <path d="M19 12h3a1 1 0 0 0 1-1V7a1 1 0 0 0-1-1h-3" />
-    <path d="M2 12h20" />
-    <path d="M12 2v2" />
-    <path d="M12 8a4 4 0 0 0-4 4" />
-    <path d="M12 8a4 4 0 0 1 4 4" />
-  </svg>
+const AppLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
+    <div className={cn("relative", className)} {...props}>
+        <Image
+            src="https://storage.googleapis.com/aifirebase-1.appspot.com/images%2Fgemini-pro-builder%2Fuser-E8tDxrC9yFhR4Y5jD6A0%2Flclt51m5%2Fgenerated_1.jpeg"
+            alt="The Harbour School Logo"
+            fill
+            className="object-contain"
+        />
+    </div>
 );
 
 export default AppLogo;
