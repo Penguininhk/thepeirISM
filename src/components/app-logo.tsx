cafdd8fe0bd577f43587c9d93a18b748387a33f0
VisorOps <pliotgaming2645@gmail.com>
2025-10-04 14:08:12 +0000

    retry with this svg, it's still broken

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const AppLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
     <div className={cn("relative", className)} {...props}>
         <Image
-            src="https://storage.googleapis.com/aifirebase-1.appspot.com/images%2Fgemini-pro-builder%2Fuser-E8tDxrC9yFhR4Y5jD6A0%2Flcq0k35t%2Fgenerated_1.png"
+            src="https://storage.googleapis.com/aifirebase-1.appspot.com/images%2Fgemini-pro-builder%2Fuser-E8tDxrC9yFhR4Y5jD6A0%2Flcsj9lfh%2Fgenerated_1.png"
             alt="The Harbour School Logo"
             fill
             className="object-contain"
