b758a19bed1724dcbeedc7413447d92fc59f8bf6
VisorOps <pliotgaming2645@gmail.com>
2025-10-04 13:46:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3430947479-ab50a",
+  "appId": "1:1054632191361:web:b2942fa594cf472d587480",
+  "apiKey": "AIzaSyDiO0wTIWWgTLA6vbc1Zv1rJZwnr1wxad8",
+  "authDomain": "studio-3430947479-ab50a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1054632191361"
+};
