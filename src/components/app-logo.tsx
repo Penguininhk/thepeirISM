23b345795fefed25604e753cb6dd93bbe1f59198
VisorOps <pliotgaming2645@gmail.com>
2025-10-04 14:10:09 +0000

    still broken.
    https://cdn.discordapp.com/attachments/101922823761378103

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const AppLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
     <div className={cn("relative", className)} {...props}>
         <Image
-            src="https://storage.googleapis.com/aifirebase-1.appspot.com/images%2Fgemini-pro-builder%2Fuser-E8tDxrC9yFhR4Y5jD6A0%2Flcsj9lfh%2Fgenerated_1.png"
+            src="https://cdn.discordapp.com/attachments/1019228237613781032/1424035545335726251/cIhuTVP.png?ex=68e27bf3&is=68e12a73&hm=6c23111c6b1d53e49e7b32f97d904d7964b474ce933e7aae2984d41a6dd059a7&"
             alt="The Harbour School Logo"
             fill
             className="object-contain"
