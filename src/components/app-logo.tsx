9e2e2970b27f5a38a9c752c26c1c9fe5db724040
VisorOps <pliotgaming2645@gmail.com>
2025-10-04 14:11:01 +0000

    mask the outer circle with a circle, it's a round logo but the square bo

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 const AppLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-    <div className={cn("relative", className)} {...props}>
+    <div className={cn("relative rounded-full overflow-hidden", className)} {...props}>
         <Image
             src="https://cdn.discordapp.com/attachments/1019228237613781032/1424035545335726251/cIhuTVP.png?ex=68e27bf3&is=68e12a73&hm=6c23111c6b1d53e49e7b32f97d904d7964b474ce933e7aae2984d41a6dd059a7&"
             alt="The Harbour School Logo"
