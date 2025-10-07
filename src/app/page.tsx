091e90c12d9c1a22ae0cfde2e213c88ecf96f258
VisorOps <pliotgaming2645@gmail.com>
2025-10-07 09:55:14 +0000

    remove the grid in the bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import AppLogo from "@/components/app-logo";
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-4">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-background bg-[linear-gradient(to_right,#e0e0e0_1px,transparent_1px),linear-gradient(to_bottom,#e0e0e0_1px,transparent_1px)] bg-[size:6rem_4rem]">
+      <div className="absolute inset-0 -z-10 h-full w-full bg-background">
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_500px_at_50%_200px,#DDE6ED,transparent)]"></div>
       </div>
       
