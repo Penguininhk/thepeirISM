7d2b88fef28262f27d3c0d9eb274df3aaff25bed
VisorOps <pliotgaming2645@gmail.com>
2025-10-07 09:47:55 +0000

    the logo is completely missing, use this as the homepage logo, still in

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -4,8 +4,9 @@ import Image from "next/image";
 const AppLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
     <div className={cn("relative rounded-full overflow-hidden", className)} {...props}>
         <Image
-            src="https://storage.googleapis.com/aifirebase/project-1-the-pier/the-harbour-school-logo.jpg"
+            src="https://cdn.discordapp.com/attachments/986847691483017248/1425056874889482353/oXbFTfm.gif?ex=68e63323&is=68e4e1a3&hm=dc9607ebcfd087fe3a421310f303b3b0df5a7d7b40adad68f3cd83cad2a41fe0&"
             alt="The Harbour School Logo"
+            unoptimized
             fill
             className="object-contain"
         />
