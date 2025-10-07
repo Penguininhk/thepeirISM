0915bb036ac543e3a8d41c8f58da9a6cc881f669
VisorOps <pliotgaming2645@gmail.com>
2025-10-07 10:07:38 +0000

    the waves are broken!!!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,13 +30,13 @@ export default function Home() {
                 </CardTitle>
               </CardHeader>
                <div className="absolute inset-0 z-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <svg className="waves-svg absolute bottom-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                <svg className="waves-svg absolute bottom-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
                   <g className="waves">
-                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="hsl(var(--accent) / 0.1)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="hsl(var(--accent) / 0.15)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="hsl(var(--accent) / 0.05)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="7" fill="hsl(var(--accent) / 0.2)" />
+                    <use href="#gentle-wave" x="48" y="0" fill="hsl(var(--accent) / 0.1)" />
+                    <use href="#gentle-wave" x="48" y="3" fill="hsl(var(--accent) / 0.15)" />
+                    <use href="#gentle-wave" x="48" y="5" fill="hsl(var(--accent) / 0.05)" />
+                    <use href="#gentle-wave" x="48" y="7" fill="hsl(var(--accent) / 0.2)" />
                   </g>
                 </svg>
               </div>
@@ -52,13 +52,13 @@ export default function Home() {
                 </CardTitle>
               </CardHeader>
               <div className="absolute inset-0 z-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <svg className="waves-svg absolute bottom-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                <svg className="waves-svg absolute bottom-0 left-0 w-full h-full" xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
                   <g className="waves">
-                    <use xlinkHref="#gentle-wave" x="48" y="0" fill="hsl(var(--accent) / 0.1)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="3" fill="hsl(var(--accent) / 0.15)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="5" fill="hsl(var(--accent) / 0.05)" />
-                    <use xlinkHref="#gentle-wave" x="48" y="7" fill="hsl(var(--accent) / 0.2)" />
+                    <use href="#gentle-wave" x="48" y="0" fill="hsl(var(--accent) / 0.1)" />
+                    <use href="#gentle-wave" x="48" y="3" fill="hsl(var(--accent) / 0.15)" />
+                    <use href="#gentle-wave" x="48" y="5" fill="hsl(var(--accent) / 0.05)" />
+                    <use href="#gentle-wave" x="48" y="7" fill="hsl(var(--accent) / 0.2)" />
                   </g>
                 </svg>
               </div>
