bfcf0509a9b196644c86d9ebefdadda0e760841b
Tan Adriel C <atan@theharbourschool.edu.hk>
2025-11-26 04:11:29 +0000

    add a circle mask around the edge

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { cn } from "@/lib/utils";
 
 const AppLogo = ({ className, ...props }: React.HTMLAttributes<HTMLDivElement>) => (
-    <div className={cn("relative", className)} {...props}>
+    <div className={cn("relative rounded-full overflow-hidden", className)} {...props}>
         <Image
             src="https://www.imghost.online/ib/YuTNsX5eknArWcm_1764130145.gif"
             alt="The Harbour School Logo"
